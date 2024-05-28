41ab0b8f22a6c6e9ae001f463add8f9196371e47
JordiFPA <167142190+JordiFPA@users.noreply.github.com>
2024-05-28 08:14:08 -0500

    Primer Commit

---
+++ ProyectoJuego/src/main/java/edu/uce/ec/ProyectoJuegoApplication.java
@@ -0,0 +1,14 @@
+package edu.uce.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJuegoApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Hola Mundo");
+		//SpringApplication.run(ProyectoJuegoApplication.class, args);
+	}
+
+}
---
+++ ProyectoJuego/src/test/java/edu/uce/ec/ProyectoJuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uce.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
