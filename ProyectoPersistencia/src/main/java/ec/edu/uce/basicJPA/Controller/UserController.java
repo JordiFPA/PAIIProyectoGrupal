c908cb7689459dc7f98854baca9a2a7d2a776515
JordiFPA <167142190+JordiFPA@users.noreply.github.com>
2024-06-09 19:12:01 -0500

    Modificación

--- ProyectoPersistencia/src/main/java/ec/edu/uce/basicJPA/Controller/UserController.java
+++ ProyectoPersistencia/src/main/java/ec/edu/uce/basicJPA/Controller/UserController.java
@@ -2,53 +2,56 @@ package ec.edu.uce.basicJPA.Controller;
 import ec.edu.uce.basicJPA.models.User;
 import ec.edu.uce.basicJPA.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
+    @RestController
+    @RequestMapping("/users")
+    public class UserController {
 
-    @Autowired
-    private UserService userService;
+        @Autowired
+        private UserService userService;
 
-    @GetMapping("/getUser")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
+        @GetMapping("/getUser")
+        public List<User> getAllUsers() {
+            return userService.getAllUsers();
+        }
 
-    @GetMapping("/getUserId/")
-    public Optional<User> getUserById(@RequestParam(name="id") long id) {
+        @GetMapping("/getUserId/")
+        public Optional<User> getUserById(@RequestParam(name="id") long id) {
 
-        return userService.getOneUser(id);
-    }
+            return userService.getOneUser(id);
+        }
 
-    @PostMapping("/createUser/")
-    public User createUser(@RequestBody User user) {
-        userService.save(user);
-        return user;
-    }
+        @PostMapping("/createUser/")
+        public User createUser(@RequestBody User user) {
+            userService.save(user);
+            return user;
+        }
 
-    @PutMapping("/updateUser/")
-    public User updateUser(@RequestParam(name="id") long id, @RequestBody User userUpDate) {
-        Optional<User> optionalUser = userService.getOneUser(id);
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-            user.setName(userUpDate.getName());
-            user.setPassword(userUpDate.getPassword());
-            user.setScore(userUpDate.getScore());
-            user.setHealth(userUpDate.getHealth());
-            return userService.updateUser(user);
-        } else {
-            return null;
+        @PutMapping("/updateUser/{id}")
+        public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+            Optional<User> optionalUser = userService.getOneUser(id);
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                user.setName(updatedUser.getName());
+                user.setPassword(updatedUser.getPassword());
+                user.setScore(updatedUser.getScore());
+                user.setHealth(updatedUser.getHealth());
+                userService.updateUser(user);
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @DeleteMapping("/deleteUser/")
+        public void deleteUser(@RequestParam(name="id") long id) {
+
+            userService.deleteUser(id);
         }
     }
-
-    @DeleteMapping("/deleteUser/")
-    public void deleteUser(@RequestParam(name="id") long id) {
-
-        userService.deleteUser(id);
-    }
-}
